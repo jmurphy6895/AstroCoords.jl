5318f4ab15195ffbf3e118fa2e12efb2d7871f7b
Jordan Murphy <jmurphy6895@gmail.com>
2024-09-21 00:18:11 -0600

    feat: aqua tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using AstroCoords
+using Aqua
 using Test
 
 @testset "AstroCoords.jl" begin
     include("test_coordinate_changes.jl")
+end
+
+
+@testset "Aqua.jl" begin
+    Aqua.test_all(AstroForceModels; ambiguities=(recursive = false))
 end
