5a328f1ebf524bd0b5765c861bd0b7bc027cfba5
Jordan Murphy <jmurphy6895@gmail.com>
2024-10-07 10:56:52 -0600

    fix: runtests missed include

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,9 @@ using Test
 
 @testset "AstroCoords.jl" begin
     include("test_coordinate_changes.jl")
+    include("test_anomalies.jl")
 end
+
 
 @testset "Aqua.jl" begin
     Aqua.test_all(AstroCoords; ambiguities=(recursive = false))
