603dcaab610857a483aba01890b98d7670d04cb8
Jordan Murphy <jmurphy6895@gmail.com>
2024-10-27 21:11:45 -0600

    feat: setup benchmark CI

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -3,19 +3,79 @@
 # ==============
 
 const SUITE = BenchmarkGroup()
-function tune_benchmarks!(g::BenchmarkGroup; seconds=nothing, gcsample=true)
-    for v in values(g)
-        if seconds !== nothing
-            v.params.seconds = seconds
-        end
-        v.params.gcsample = gcsample
-        v.params.evals = 1 # `setup` must be functional
-    end
+
+SUITE["transformation"] = BenchmarkGroup(["convert"])
+SUITE["anomalies"] = BenchmarkGroup(["keplerian math"])
+SUITE["quantities"] = BenchmarkGroup(["derived"])
+
+const _COORDINATE_SETS = [
+    Cartesian,
+    Delaunay,
+    Keplerian,
+    Milankovich,
+    ModEq,
+    Cylindrical,
+    Spherical,
+    USM7,
+    USM6,
+    USMEM,
+]
+
+const _state = [
+    -1076.225324679696
+    -6765.896364327722
+    -332.3087833503755
+    9.356857417032581
+    -3.3123476319597557
+    -1.1880157328553503
+]
+
+const _μ = 3.986004415e5
+
+const _cart_state = Cartesian(state)
+
+for set in _COORDINATE_SETS
+    suite["transformation"][string(set), _cart_state, _μ] = @benchmarkable $(f)(
+        $_cart_state, $_μ
+    )
+    new_coord = set(_cart_state, _μ)
+    suite["transformation"][string(set) * "reverse", new_coord, _μ] = @benchmarkable Cartesian(
+        $new_coord, $_μ
+    )
 end
 
-# warm up
+const _anomaly_conversions = [
+    meanAnomaly2EccentricAnomaly,
+    meanAnomaly2TrueAnomaly,
+    trueAnomaly2EccentricAnomaly,
+    trueAnomaly2MeanAnomaly,
+    eccentricAnomaly2MeanAnomaly,
+    eccentricAnomaly2TrueAnomaly,
+]
 
-# analysis performance
-# --------------------
+const _e = 0.30230575359641376
+const _angle = 0.6441434680007933
 
-#TODO: SEE JET.jl
+for f in _anomaly_conversions
+    suite["anomalies"][string(f), _angle, _e] = $(f)($_angle, $_e)
+end
+
+const _quantity_functions = [
+    meanMotion, orbitalPeriod, orbitalNRG, angularMomentumVector, angularMomentumQuantity
+]
+
+for f in _anomaly_conversions
+    suite["quantities"][string(f), _cart_state, _μ] = $(f)($_cart_state, $_μ)
+end
+
+# If a cache of tuned parameters already exists, use it, otherwise, tune and cache
+# the benchmark parameters. Reusing cached parameters is faster and more reliable
+# than re-tuning `suite` every time the file is included.
+paramspath = joinpath(dirname(@__FILE__), "params.json")
+
+if isfile(paramspath)
+    loadparams!(suite, BenchmarkTools.load(paramspath)[1], :evals)
+else
+    tune!(suite)
+    BenchmarkTools.save(paramspath, params(suite))
+end
